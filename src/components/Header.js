33c0ffd49169409324080b05b4ea16893dba3ba7
Aparna Yadav <asy.yadav97@gmail.com>
2019-05-25 00:35:38 +0530

    Add a header

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import LogoutButton from './Auth/LogoutButton';
+
+const Header = (props) => {
+    return (
+        <div className="ui secondary pointing menu">
+            TODO
+            <div className="right menu">
+                <div className="item"> 
+                    <LogoutButton {...props} />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
